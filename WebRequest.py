3a437b4e2d74905e2850cd9de06def713371f49f
Will Schultz <schultzwc@yahoo.com>
2019-06-07 21:46:30 -0500

    Fully functional script. Ready for a cron job schedule

--- WebRequest.py
+++ WebRequest.py
@@ -3,7 +3,6 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 import re
-import lxml
 import datetime
 import smtplib
 from email.message import EmailMessage
@@ -42,17 +41,23 @@ def check_release(wildcard):
 
 def send_mail():
     port =  465
+    email = 'wcschultz42@gmail.com'
     password = '2433006Ws'
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
-        #server.starttls()
-        server.login('wcschultz42@gmail.com', '2433006Ws')
-        server.sendmail('wcschultz42@gmail.com', '6124994263@mms.att.net', 'Test Email')
-        server.close()
+        msg = EmailMessage()
+        msg.set_content('A new version of Power BI has been released.')
+        msg['Subject'] = 'New Power BI Update'
+        msg['From'] = email
+        msg['To'] = '6124994263@tmomail.net'
+        server.login(email, password)
+        server.send_message(msg)
+        server.quit()
+
 
 now = datetime.datetime.now()
 wildcard = str(now.month) + '/../' + str(now.year)
 result = check_release(wildcard)
 
-if result == 'no match':
+if result == 'match':
     send_mail()
