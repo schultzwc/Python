2011fc8c3fe04d39e37b4e4bab9c1bb9b7489fbb
Will Schultz <schultzwc@yahoo.com>
2019-06-07 21:21:54 -0500

    First version of Power BI Data scraping

---
+++ WebRequest.py
@@ -0,0 +1,58 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import re
+import lxml
+import datetime
+import smtplib
+from email.message import EmailMessage
+import ssl
+
+def simple_get(url):
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}', format(url, str(e)))
+        return None
+
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 and content_type is not None and content_type.find('html') > -1)
+
+def log_error(e):
+    print(e)
+
+def check_release(wildcard):
+    website = 'https://docs.microsoft.com/en-us/power-bi/desktop-latest-update'
+    page = simple_get(website)
+    page_parse = BeautifulSoup(page, 'lxml')
+    release_date_line = page_parse.find('meta', {'name':'ms.date'})
+    release_date = release_date_line['content']
+    date_wildcard = re.compile(wildcard)
+
+    if date_wildcard.findall(release_date):
+        return 'match'
+    else:
+        return 'no match'
+
+def send_mail():
+    port =  465
+    password = '2433006Ws'
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+        #server.starttls()
+        server.login('wcschultz42@gmail.com', '2433006Ws')
+        server.sendmail('wcschultz42@gmail.com', '6124994263@mms.att.net', 'Test Email')
+        server.close()
+
+now = datetime.datetime.now()
+wildcard = str(now.month) + '/../' + str(now.year)
+result = check_release(wildcard)
+
+if result == 'no match':
+    send_mail()
